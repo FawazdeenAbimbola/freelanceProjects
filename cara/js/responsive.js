23092d1f74b171b3505b23b6b2fbdad8114a91a7
Fawaz <Bigabimbola@gmail.com>
2022-12-01 20:16:20 +0100

    Added design, responsiveness and transitions

---
+++ cara/js/responsive.js
@@ -0,0 +1,7 @@
+ function openNav(){
+    document.getElementById('responsive').style.width  = "100%";
+ }
+ 
+ function closeNav(){
+    document.getElementById('responsive').style.width  = "0%";
+ }
